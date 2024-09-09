0db0bfea2bde9bf92b571b8ace7fc65518f65964
Mujtaba-dev56 <ahmedmujtaba12450@gmail.com>
2024-09-09 16:39:55 -0700

    events

---
+++ WindowEvent.js
@@ -0,0 +1,39 @@
+ document.getElementById('button4').addEventListener
+ ('mouseover', ()=> {
+    alert('You are welcome')
+    
+ })
+
+ document.getElementById('button4').addEventListener
+ ('click', ()=> {
+    alert('You are welcome')
+    
+ })
+
+ document.getElementById('button4').addEventListener
+ ('dblclick', ()=> {
+    alert('You are welcome')
+    
+ })
+
+//  document.getElementById('button5').addEventListener
+//  ('click', ()=> {
+    
+//     window.open()
+//  })
+
+
+// give url
+
+document.getElementById('button5').addEventListener
+('click', ()=> {
+   
+   window.open('http://gogle.com')
+})
+
+
+document.getElementById('button6').addEventListener
+('click', ()=> {
+   
+   window.close()
+})
